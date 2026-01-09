2e618049a932c49af0a66dc6f63b5dc691c993dc
rohit1302 <rohitnew1302@gmail.com>
2026-01-09 23:21:32 +0530

    started with react prctice

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Hello from "./components/helloworld";
+function App() {
+  return (
+      <Hello/>
+  );
+}
+
+export default App;
---
+++ src/components/helloworld.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+    return(
+        <div className="hello">
+            <h1>Hello World</h1>
+            <p >My name is Rohit</p>
+        </div>
+    )
+};
+export default Hello;
